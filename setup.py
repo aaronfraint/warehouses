855c1722268bf2ae6c9c60a9d47b95c6df2b0cfb
aaronfraint <aaronfraint@icloud.com>
2022-05-12 16:39:52 -0400

    initial boilerplate

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="warehouses",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Python library to facilitate read/write of GIS and tabular data between Python and cloud warehouses",
+    author="Aaron Fraint, AICP",
+    license="MIT",
+)
