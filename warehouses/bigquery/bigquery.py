2627b0bf1a38c1c2a36600600bc1d0e31c87e0d6
Aaron Fraint <aaronfraint@icloud.com>
2022-07-06 15:03:40 -0400

    update connections to use sevice account files

--- warehouses/bigquery/bigquery.py
+++ warehouses/bigquery/bigquery.py
@@ -1,28 +1,94 @@
+import os
+import pydata_google_auth
+from google.cloud import bigquery
 from dataclasses import dataclass
 from pandas import DataFrame, read_gbq
+from dotenv import load_dotenv, find_dotenv
 from geopandas import GeoDataFrame, GeoSeries
 
-from .auth import pydata_auth, gcloud_auth
+load_dotenv(find_dotenv())
+
+
+def connect_to_gcloud(*args, **kwargs) -> bigquery.Client:
+    """
+    Connect to BigQuery:
+        1. Prefer the user-provided service account JSON file
+        2. If none provided, read from environment
+        3. Otherwise, connect normally
+    """
+
+    if "GCP_SERVICE_ACCOUNT_JSON" in kwargs:
+        json_path = kwargs["GCP_SERVICE_ACCOUNT_JSON"]
+        kwargs.pop("GCP_SERVICE_ACCOUNT_JSON")
+        return bigquery.Client.from_service_account_json(json_path, *args, **kwargs)
+
+    GCP_SERVICE_ACCOUNT_JSON = os.getenv("GCP_SERVICE_ACCOUNT_JSON")
+    if GCP_SERVICE_ACCOUNT_JSON:
+        return bigquery.Client.from_service_account_json(
+            GCP_SERVICE_ACCOUNT_JSON, *args, **kwargs
+        )
+
+    else:
+        return bigquery.Client(*args, **kwargs)
+
+
+def pydata_credentials(GCP_SERVICE_ACCOUNT_JSON: str | None = None):
+    """Create credentials that are used by `pandas.read_gqb()`"""
+
+    scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    # Use argument if provided
+    if GCP_SERVICE_ACCOUNT_JSON:
+        return pydata_google_auth.load_service_account_credentials(
+            GCP_SERVICE_ACCOUNT_JSON,
+            scopes,
+        )
+
+    # Try an environment variable next
+    GCP_SERVICE_ACCOUNT_JSON = os.getenv("GCP_SERVICE_ACCOUNT_JSON")
+    if GCP_SERVICE_ACCOUNT_JSON:
+        return pydata_google_auth.load_service_account_credentials(
+            GCP_SERVICE_ACCOUNT_JSON,
+            scopes,
+        )
+
+    # Otherwise, connect as a user
+    return pydata_google_auth.get_user_credentials(
+        scopes,
+        # Set auth_local_webserver to True to have a slightly more convienient
+        # authorization flow. Note, this doesn't work if you're running from a
+        # notebook on a remote sever, such as over SSH or with Google Colab.
+        auth_local_webserver=True,
+    )
 
 
 @dataclass
 class BQWarehouse:
 
     project_id: str
+    bq_connection: dict = {}
     credentials = None
     client = None
 
     @property
     def pydata_creds(self):
         if not self.credentials:
-            self.credentials = pydata_auth()
+            if "GCP_SERVICE_ACCOUNT_JSON" in self.bq_connection:
+                self.credentials = pydata_credentials(
+                    self.bq_connection["GCP_SERVICE_ACCOUNT_JSON"]
+                )
+            else:
+                self.credentials = pydata_credentials()
 
         return self.credentials
 
     @property
     def gcloud_client(self):
         if not self.client:
-            self.client = gcloud_auth()
+            self.client = connect_to_gcloud(**self.bq_connection)
 
         return self.client
 
@@ -79,3 +145,5 @@ class BQWarehouse:
             **kwargs,
         )
         return job.result()
+
+    ## TODO: execute query
